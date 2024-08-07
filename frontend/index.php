723982045d9639c30c064fcedb08b1f25350aa74
P A DAHAM KAUSHIKA LAKSHMAN <116593030+DKLakshman@users.noreply.github.com>
2024-08-07 12:20:58 +0530

    home page section 1 sketch

--- frontend/index.php
+++ frontend/index.php
@@ -1,34 +1,55 @@
     <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SEDS Header</title>
+    <html lang="en">
 
-    <!-- tab icon -->
-    <link rel="icon" type="image/png" href="resources/images/header/web_icon.png">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>SEDS Header</title>
 
-    <!-- link css -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/bootstrap.css">
-    <link rel="stylesheet" href="css/main.css">
-    <link rel="stylesheet" href="css/style.css">
+        <!-- tab icon -->
+        <link rel="icon" type="image/png" href="resources/images/header/web_icon.png">
 
-    <!-- fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Passion+One:wght@400;700;900&display=swap" rel="stylesheet">
+        <!-- link css -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/style.css">
 
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Lexend+Exa:wght@100..900&display=swap" rel="stylesheet">
+        <!-- fonts -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Passion+One:wght@400;700;900&display=swap" rel="stylesheet">
 
-    <!-- fontawsom -->
-    <script src="https://kit.fontawesome.com/79961d807c.js" crossorigin="anonymous"></script>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Lexend+Exa:wght@100..900&display=swap" rel="stylesheet">
 
-    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-</head>
-<body>
-    
-</body>
-</html>
+        <!-- fontawsom -->
+        <script src="https://kit.fontawesome.com/79961d807c.js" crossorigin="anonymous"></script>
+
+        <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    </head>
+
+    <body>
+        <!-- section 1(seds-home-s1) -->
+        <section class="seds-home-s1">
+            <div class="container d-flex justify-content-center align-items-center h-100">
+                <div class="seds-home-s1-bg d-flex">
+                    <div class="seds-home-s1-ball"></div>
+                    <div class="seds-home-s1-astronet"></div>
+                </div>
+                <div class="seds-home-s1-header-box text-light">
+                    <div class="seds-home-s1-header alg-text-h1 passion-one-black">
+                        <div class="seds-home-s1-header-p1">WELCOME TO SEDS SLIIT</div>
+                        <div class="seds-home-s1-header-p2"></div>
+                        <div class="seds-home-s1-header-p3"></div>
+                    </div>
+                    <div class="seds-home-s1-para1 passion-one-bold alg-text-h2">Students for the Exploration and Development of Space</div>
+                    <div class="seds-home-s1-para2 passion-one-regular ">Sri Lanka Institute of Information Technology</div
+            </div>
+        </section>
+
+
+    </body>
+
+    </html>
