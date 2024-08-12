fba6f8edfa2088d24fe3d14b0cc93b5f685150b5
W S S De Silva <wsasanka.shenal@gmail.com>
2024-08-13 00:01:44 +0530

    WRONG ABOUT

---
+++ frontend/pages/aboutUs.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SEDS | SLIIT</title>
+
+    <!-- tab icon -->
+    <link rel="icon" type="image/png" href="../resources/images/header/web_icon.png">
+
+    <!-- link css -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/bootstrap.css">
+    <link rel="stylesheet" href="../css/main.css">
+    <link rel="stylesheet" href="../css/style.css">
+
+    <!-- fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Passion+One:wght@400;700;900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Lexend+Exa:wght@100..900&display=swap" rel="stylesheet">
+
+    <!-- FontAwesome -->
+    <script src="https://kit.fontawesome.com/79961d807c.js" crossorigin="anonymous"></script>
+
+    <!-- Bootstrap JavaScript -->
+    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body>
+    <?php 'header.php';?>
+
+<section class="seds-aboutUs-s1">
+        <div class="container seds-aboutUs-s1-container ">
+        
+            <div class="seds-aboutUs-s2-left alg-text-light passion-one-black">
+                <div class="seds-aboutUs-s2-left-h1 alg-text-h1 ">ABOUT US</div>
+                <div class="seds-aboutUs-s2-left-para alg-text-h3 pb-4">SEDS SLIT is the Student Organization for the Exploration and Development of Space at Sri Lanka Institute of Technology
+
+SEDS SLIT is a university chapter of the global organization SEDS (Students for the Exploration and Development of Space). The chapter aims to promote space exploration and development through various educational, research, and outreach activities.
+
+SEDS SLIIT founded by a group led by Saditha Dissanayaka and Ravindu Piyapema, became  an approved chapter on July 4, 2019. Aligned with SEDS SL vision, the chapter actively engages in projects, events, and competitions, contributing to aerospace innovation</div>
+                <div class="seds-home-s2-left-button-box">
+                    <button class="seds-home-s2-left-button alg-bolder alg-rounded-small border-0 alg-secondary-gradient-hori" type="submit">Read More</button>
+                </div>
+            </div>
+        </div>
+        </section>
+</body>
+
+</html>
