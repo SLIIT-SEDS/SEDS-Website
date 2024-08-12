b84085f89c2208a1bc335e70bd98eb6d5638a20e
W S S De Silva <wsasanka.shenal@gmail.com>
2024-08-13 00:46:33 +0530

    correct about us

--- frontend/pages/aboutUs.php
+++ frontend/pages/aboutUs.php
@@ -14,6 +14,7 @@
     <link rel="stylesheet" href="../css/bootstrap.css">
     <link rel="stylesheet" href="../css/main.css">
     <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/aboutUs.css">
 
     <!-- fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -32,20 +33,18 @@
     <?php include 'header.php';?>
 
 <section class="seds-aboutUs-s1">
-        <div class="container seds-aboutUs-s1-container ">
-        
-            <div class="seds-aboutUs-s2-left alg-text-light passion-one-black">
-                <div class="seds-aboutUs-s2-left-h1 alg-text-h1 ">ABOUT US</div>
-                <div class="seds-aboutUs-s2-left-para alg-text-h3 pb-4">SEDS SLIT is the Student Organization for the Exploration and Development of Space at Sri Lanka Institute of Technology
+<div class="container seds-aboutUs-s1-container">
+    <div class="seds-home-s2-left alg-text-light passion-one-black">
+        <div class="seds-home-s2-left-h1 alg-text-h1">ABOUT US</div>
+        <div class="seds-home-s2-left-para alg-text-h3 pb-4">SEDS SLIT is the Student Organization for the Exploration and Development of Space at Sri Lanka Institute of Technology.</div>
+        <div class="seds-home-s2-left-para alg-text-h3 pb-4">SEDS SLIT is a university chapter of the global organization SEDS (Students for the Exploration and Development of Space). The chapter aims to promote space exploration and development through various educational, research, and outreach activities.</div>
+        <div class="seds-home-s2-left-para alg-text-h3 pb-4">SEDS SLIIT founded by a group led by Saditha Dissanayaka and Ravindu Piyapema, became an approved chapter on July 4, 2019. Aligned with SEDS SL vision, the chapter actively engages in projects, events, and competitions, contributing to aerospace innovation.</div>
+    </div>
+    <div class="seds-aboutUs-image">
+        <img src="../resources/images/aboutUs/rocketab.png" alt="rocket image">
+    </div>
+</div>
 
-SEDS SLIT is a university chapter of the global organization SEDS (Students for the Exploration and Development of Space). The chapter aims to promote space exploration and development through various educational, research, and outreach activities.
-
-SEDS SLIIT founded by a group led by Saditha Dissanayaka and Ravindu Piyapema, became  an approved chapter on July 4, 2019. Aligned with SEDS SL vision, the chapter actively engages in projects, events, and competitions, contributing to aerospace innovation</div>
-                <div class="seds-home-s2-left-button-box">
-                    <button class="seds-home-s2-left-button alg-bolder alg-rounded-small border-0 alg-secondary-gradient-hori" type="submit">Read More</button>
-                </div>
-            </div>
-        </div>
         </section>
 </body>
 
