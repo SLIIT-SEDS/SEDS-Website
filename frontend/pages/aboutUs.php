c2d401efe17ecd6036595cc4a576fba9b0554026
W S S De Silva <wsasanka.shenal@gmail.com>
2024-08-16 01:12:17 +0530

    about us done

--- frontend/pages/aboutUs.php
+++ frontend/pages/aboutUs.php
@@ -35,16 +35,58 @@
     <section class="seds-aboutUs-s1" id="abt-section">
         <div class="container seds-aboutUs-s1-container">
             <div class="seds-home-s2-left alg-text-light passion-one-black">
-                <div class="seds-home-s2-left-h1 alg-text-h1">ABOUT US</div>
-                <div class="seds-home-s2-left-para alg-text-h3 pb-4">SEDS SLIT is the Student Organization for the Exploration and Development of Space at Sri Lanka Institute of Technology.</div>
-                <div class="seds-home-s2-left-para alg-text-h3 pb-4">SEDS SLIT is a university chapter of the global organization SEDS (Students for the Exploration and Development of Space). The chapter aims to promote space exploration and development through various educational, research, and outreach activities.</div>
-                <div class="seds-home-s2-left-para alg-text-h3 pb-4">SEDS SLIIT founded by a group led by Saditha Dissanayaka and Ravindu Piyapema, became an approved chapter on July 4, 2019. Aligned with SEDS SL vision, the chapter actively engages in projects, events, and competitions, contributing to aerospace innovation.</div>
+                <div class="seds-home-s2-left-h1 alg-text-main-gradient-verti alg-text-h1">ABOUT US</div>
+                <div class="seds-home-s2-left-para alg-text-h3 pb-4">SEDS SLIT is the Student Organization for the
+                    Exploration and Development of Space at Sri Lanka Institute of Technology.</div>
+                <div class="seds-home-s2-left-para alg-text-h3 pb-4">SEDS SLIT is a university chapter of the global
+                    organization SEDS (Students for the Exploration and Development of Space). The chapter aims to
+                    promote space exploration and development through various educational, research, and outreach
+                    activities.</div>
+                <div class="seds-home-s2-left-para alg-text-h3 pb-4">SEDS SLIIT founded by a group led by Saditha
+                    Dissanayaka and Ravindu Piyapema, became an approved chapter on July 4, 2019. Aligned with SEDS SL
+                    vision, the chapter actively engages in projects, events, and competitions, contributing to
+                    aerospace innovation.</div>
             </div>
             <div class="seds-aboutUs-image">
-                <img src="../resources/images/aboutUs/rocketab.png" alt="rocket image">
+                <img src="../resources/images/aboutUs/rocketworld.png" alt="rocketwrld image">
             </div>
         </div>
+        <section class="seds-aboutUs-s1" id="abt-section">
+            <div class="container seds-aboutUs-s1-container">
+                <div class="seds-aboutUs-image">
+                    <img src="../resources/images/aboutUs/BLUEERTH.png" alt="blueworld">
+                </div>
+                <div class="seds-home-s2-left alg-text-light passion-one-black">
+                    <div class="seds-home-s2-left-h1 alg-text-main-gradient-verti alg-text-h1">OUR TEM</div>
+                    <div class="seds-home-s2-left-para alg-text-h3 pb-4"
+                        style="display: flex; justify-content: space-between;">
+                        <span>President</span>
+                        <span>Sudeepa Jayanath</span>
+                    </div>
+                    <div class="seds-home-s2-left-para alg-text-h3 pb-4"
+                        style="display: flex; justify-content: space-between;">
+                        <span>Secretary</span>
+                        <span>Dewdu Sendanayake</span>
+                    </div>
+                    <div class="seds-home-s2-left-para alg-text-h3 pb-4"
+                        style="display: flex; justify-content: space-between;">
+                        <span>Treasurer</span>
+                        <span>Himesha Senaratne</span>
+                    </div>
+                    <div class="seds-home-s2-left-para alg-text-h3 pb-4"
+                        style="display: flex; justify-content: space-between;">
+                        <span>Publication Manager</span>
+                        <span>Binuri Liyanage</span>
+                    </div>
+                    <div class="seds-home-s2-left-para alg-text-h3 pb-4"
+                        style="display: flex; justify-content: space-between;">
+                        <span>Editor</span>
+                        <span>Chamika Lakshan</span>
+                    </div>
+                </div>
+            </div>
 
+        </section>
     </section>
 </body>
 
