14fea447b85b8e70322790cf2d7dfd6cd5e70bb6
P A DAHAM KAUSHIKA LAKSHMAN <116593030+DKLakshman@users.noreply.github.com>
2024-08-16 22:46:48 +0530

    hover effects

--- frontend/pages/header.php
+++ frontend/pages/header.php
@@ -49,7 +49,7 @@
                             <a class="nav-link text-light alg-text-h3 alg-bold lexend-exa" href="../pages/aboutUs.php">ABOUT US</a>
                         </li>
                         <li class="nav-item d-flex align-items-center" >
-                            <button class="seds-header-button lexend-exa d-flex justify-content-center alg-bold align-items-center text-light alg-rounded-small alg-text-p" type="submit" > <a href="">JOIN WITH US</a></button>
+                            <button class="seds-header-button lexend-exa d-flex justify-content-center alg-bold align-items-center text-light alg-rounded-small alg-text-p" type="submit" > <a href="https://forms.gle/BGyKgMRVqPeJY2WM9">JOIN WITH US</a></button>
                         </li>
                     </ul>
                 </div>
